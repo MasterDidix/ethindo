12aef9f7473c1eb51a1f237c4f78dcfac167abc3
MasterDidix <46417057+MasterDidix@users.noreply.github.com>
2019-12-12 08:20:07 +0700

    Add files via upload

---
+++ ethindo.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "
+ _____                  _    ___        _ _
+|_   _|   _ _   _ _   _| |  / _ \ _ __ | (_)_ __   ___
+  | || | | | | | | | | | | | | | | '_ \| | | '_ \ / _ \
+  | || |_| | |_| | |_| | | | |_| | | | | | | | | |  __/
+  |_| \__,_|\__, |\__,_|_|  \___/|_| |_|_|_|_| |_|\___|
+            |___/\n";
+echo " Tutor  : Master D\n";
+echo " Chanel : Tuyul Online \n";
+echo " Watsapp: 085736292046 \n";
+echo " Link Group : https://chat.whatsapp.com/HjKGAz5pULU7nEwlJeIEVl\n";
+echo " Chanel Youtube :https://www.youtube.com/channel/UC-Gh087RlYvvOeNnBKQ6tDw \n";
+echo "==============================================================\n";
+echo "Program Nuyul: ethindo\n";
+echo "Ini Program Ilegal, Resiko Tanggung Sendiri\n";
+echo "==============================================================\n";
+echo "Login Success\n";
+echo "Welcome To ethindo Bot ".$js["user"]["name"]."\n";
+echo "Your Ballance : ".$js["user"]["user_balance"]."\n";
+echo "\n\nStart Claiming.....!\n";
+
+
+?>
